de87bf04c0b756cb14d46e887760d6a2d9ad0357
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-02-23 23:00:52 +0100

    fix: fetching data

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,13 +1,13 @@
-import {doublePrecision, pgTable, serial, text, timestamp} from "drizzle-orm/pg-core";
+import {date, doublePrecision, pgTable, serial, text, timestamp} from "drizzle-orm/pg-core";
 
 export const costsData = pgTable("costsData", {
     id: serial("id").primaryKey(),
-    createdAt: timestamp("createdAt").default(new Date()),
+    createdAt: timestamp("created_at").default(new Date()),
     type: text("type").notNull(),
     details: text("details").notNull(),
     currency: text("currency").notNull(),
     total: doublePrecision("total").notNull(),
-    date: timestamp("date").notNull(),
+    date: date("date").notNull(),
     email: text("email").notNull(),
     carBrand: text("carBrand").notNull(),
     carName: text("carName").notNull(),

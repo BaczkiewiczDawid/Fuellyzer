43975e194eaee85c3451a018a49e4a732b16d803
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-02-26 17:21:00 +0100

    feat: car selector

--- frontend/app/(tabs)/new-cost.tsx
+++ frontend/app/(tabs)/new-cost.tsx
@@ -2,11 +2,13 @@ import {Wrapper} from "@/components/wrapper";
 import {Title} from "@/components/title";
 import {Description} from "@/components/description";
 import {Navigation} from "@/components/navigation";
-import {useEffect, useState} from "react";
-import {View, StyleSheet, Pressable, Text} from "react-native";
+import {ReactNode, useEffect, useState} from "react";
+import {View, StyleSheet, Pressable, Text, TouchableOpacity} from "react-native";
 import {useApi} from "@/hooks/useApi";
 import {Car} from "@/types/car";
 import {FormLabel} from "@/components/form-label";
+import {Ionicons} from "@expo/vector-icons";
+import Svg, {Path, Circle} from "react-native-svg";
 
 export default function NewCost() {
     const [activeView, setActiveView] = useState("Refuel");
@@ -19,6 +21,8 @@ export default function NewCost() {
     const [cost, setCost] = useState<number | undefined>(undefined)
     const [fullRefuel, setFullRefuel] = useState<boolean>(true)
     const [error, setError] = useState<boolean>(false)
+    const [dropdownOpen, setDropdownOpen] = useState<boolean>(false)
+    const [selectedCar, setSelectedCar] = useState<any>(undefined)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -37,6 +41,12 @@ export default function NewCost() {
 
     const defaultSelectedCar = availableCars.find((car: Car) => car.defaultSelected)
 
+    useEffect(() => {
+        if (defaultSelectedCar) {
+            setSelectedCar(defaultSelectedCar.carName)
+        }
+    }, [defaultSelectedCar]);
+
     const handleNewCost = () => {
         if (!mileage || !fuelAmount || !price || !cost) {
             setError(true)
@@ -50,14 +60,36 @@ export default function NewCost() {
             <Description>Fill in the form below to add a new cost</Description>
             <View style={styles.formWrapper}>
                 <Navigation activeView={activeView} setActiveView={setActiveView} views={views}/>
-                <select style={styles.select}
-                        defaultValue={defaultSelectedCar ? defaultSelectedCar?.carBrand + defaultSelectedCar?.carName : ""}>
-                    {availableCars.map((car: Car, index: number) => {
-                        return (
-                            <option key={index} value={car.carBrand + car.carName}>{car.carBrand} {car.carName}</option>
-                        )
-                    })}
-                </select>
+                <View style={styles.vehicleContainer}>
+                    {availableCars.map((car) => (
+                        <TouchableOpacity
+                            key={car.id}
+                            style={[styles.carOption, selectedCar === car.carName && styles.selected]}
+                            onPress={() => setSelectedCar(car.carName)}
+                        >
+                            <View style={styles.carIcon}>
+                                <Svg width={20} height={20} viewBox="0 0 24 24" stroke="black" strokeWidth={2}
+                                     fill="none">
+                                    <Path
+                                        d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.5 2.8C1.4 11.4 1 12.3 1 13.2V16c0 .6.4 1 1 1h2"/>
+                                    <Circle cx={7} cy={17} r={2}/>
+                                    <Path d="M9 17h6"/>
+                                    <Circle cx={17} cy={17} r={2}/>
+                                </Svg>
+                            </View>
+                            <View style={styles.carDetails}>
+                                <Text
+                                    style={styles.carName}>{car.carBrand} {car.carName}</Text>
+                            </View>
+                            {selectedCar === car.carName && (
+                                <Svg width={20} height={20} viewBox="0 0 24 24" stroke="black" strokeWidth={2}
+                                     fill="none">
+                                    <Path d="M20 6L9 17l-5-5"/>
+                                </Svg>
+                            )}
+                        </TouchableOpacity>
+                    ))}
+                </View>
                 <form style={styles.form}>
                     <FormLabel title={"Mileage"} placeholder={"Mileage..."} type={"number"} value={mileage}
                                setValue={setMileage} error={error}/>
@@ -103,5 +135,45 @@ const styles = StyleSheet.create({
     buttonText: {
         color: "#fafafa",
         textAlign: "center",
+    },
+    vehicleSelector: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#1E88E5',
+        padding: 14,
+        marginHorizontal: 16,
+        marginTop: 16,
+        borderRadius: 4,
+    },
+    vehicleSelectorText: {
+        color: 'white',
+        fontWeight: '500',
+    },
+    carOption: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    selected: {
+        borderColor: "#0F77F0",
+        borderWidth: 1,
+    },
+    carIcon: {
+        marginRight: 10
+    },
+    carDetails: {
+        flex: 1
+    },
+    carName: {
+        fontSize: 16, fontWeight: "bold"
+    },
+    carInfo: {
+        fontSize: 14, color: "gray"
+    },
+    vehicleContainer: {
+        marginTop: 30,
     }
 })
--- frontend/components/form-label.tsx
+++ frontend/components/form-label.tsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, TextInput, KeyboardTypeOptions} from "react-native";
 
 type Props = {
     title: string,
@@ -10,11 +10,26 @@ type Props = {
 }
 
 export const FormLabel = ({title, placeholder, type, value, setValue, error}: Props) => {
+    let keyboardType: KeyboardTypeOptions = "default"
+
+    if (type === "number") {
+        keyboardType = "numeric"
+    }
+    if (type === "email") {
+        keyboardType = "email-address"
+    }
+
     return (
         <View>
-            <label><Text style={styles.label}>{title}</Text></label>
-            <input placeholder={placeholder} style={styles.input} type={type} value={value}
-                   onChange={(event) => setValue(event.target.value)}/>
+            <Text style={styles.label}>{title}</Text>
+            <TextInput
+                placeholder={placeholder}
+                style={styles.input}
+                keyboardType={keyboardType}
+                value={value?.toString()}
+                secureTextEntry={type === 'password'}
+                onChangeText={(text) => setValue(text)}
+            />
             {error && !value && <Text style={styles.error}>This field is required</Text>}
         </View>
     )
@@ -24,16 +39,16 @@ const styles = StyleSheet.create({
     label: {
         color: "#101D1E",
         fontSize: 16,
-        fontWeight: "regular"
+        fontWeight: "400"
     },
     input: {
-        width: "92%",
+        width: "100%",
         padding: 10,
         borderRadius: 10,
         marginTop: 10,
         borderWidth: 2,
         borderColor: "#D9D9D9",
-        fontWeight: "light",
+        fontWeight: "300",
     },
     error: {
         color: "#FF3D3D",

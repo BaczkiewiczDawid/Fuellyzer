b811a88d6c42a25fdd413f6b86779f0f00c872a9
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-04-01 12:59:51 +0200

    feat: details view

--- frontend/app/(tabs)/fueling-log.tsx
+++ frontend/app/(tabs)/fueling-log.tsx
@@ -1,4 +1,4 @@
-import { Text } from "react-native"
+import {StyleSheet, Text, View} from "react-native"
 import {Wrapper} from "@/components/wrapper";
 import {Title} from "@/components/title";
 
@@ -6,6 +6,51 @@ export default function FuelingLog() {
     return (
         <Wrapper>
             <Title>Fueling history</Title>
+            <View style={styles.historyItemWrapper}>
+                <View style={styles.mainInfoWrapper}>
+                    <View>
+                        <Text style={styles.bold}>24-01-2025</Text>
+                        <Text>$65.43</Text>
+                    </View>
+                    <View>
+                        <Text style={styles.bold}>147 954km</Text>
+                        <Text>+412km</Text>
+                    </View>
+                </View>
+                <View style={styles.detailsWrapper}>
+                    <View style={styles.detailsWrapper}>
+                        <Text>PB95 $1.64</Text>
+                        <Text>12.5 l/100</Text>
+                    </View>
+                </View>
+            </View>
         </Wrapper>
     )
 }
+
+const styles = StyleSheet.create({
+    historyItemWrapper: {
+        flexDirection: "column",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: "#E5E5E5",
+        backgroundColor: 'white',
+    },
+    mainInfoWrapper: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+    },
+    detailsWrapper: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+        marginTop: 12,
+    },
+    bold: {
+        fontWeight: "bold",
+    }
+})

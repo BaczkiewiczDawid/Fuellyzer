0d5b45073bb8711ac81a2138957307a8f9535537
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-04-01 12:42:05 +0200

    feat: fueling log page

---
+++ frontend/app/(tabs)/fueling-log.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native"
+import {Wrapper} from "@/components/wrapper";
+import {Title} from "@/components/title";
+
+export default function FuelingLog() {
+    return (
+        <Wrapper>
+            <Title>Fueling history</Title>
+        </Wrapper>
+    )
+}

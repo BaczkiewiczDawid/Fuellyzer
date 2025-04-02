9a3275aeeb722821ba1829508f04148d6ae0fb95
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-04-02 18:17:02 +0200

    feat: fetch fueling history

--- frontend/app/(tabs)/fueling-log.tsx
+++ frontend/app/(tabs)/fueling-log.tsx
@@ -1,29 +1,63 @@
 import {StyleSheet, Text, View} from "react-native"
 import {Wrapper} from "@/components/wrapper";
 import {Title} from "@/components/title";
+import {useApi} from "@/hooks/useApi";
+import {useEffect, useState} from "react";
+import {Car} from "@/types/car";
 
 export default function FuelingLog() {
+    const [carsDataHistory, setCarsDataHistory] = useState([])
+    const [selectedCarDataHistory, setSelectedCarDataHistory] = useState([])
+    const [selectedCar, setSelectedCar] = useState({
+        carBrand: "Volkswagen",
+        carName: "Golf VII",
+    })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await useApi("http://localhost:4000/history", "GET");
+                const data = await response;
+                setCarsDataHistory(data);
+            } catch (error) {
+                console.error("Error fetching fueling history:", error);
+            }
+        }
+
+        fetchData();
+    }, [])
+
+    useEffect(() => {
+        setSelectedCarDataHistory(carsDataHistory.filter((car: Car) => car.carBrand === selectedCar.carBrand && car.carName === selectedCar.carName))
+    }, [selectedCar]);
+
+    console.log(selectedCarDataHistory)
+
     return (
         <Wrapper>
             <Title>Fueling history</Title>
-            <View style={styles.historyItemWrapper}>
-                <View style={styles.mainInfoWrapper}>
-                    <View>
-                        <Text style={styles.bold}>24-01-2025</Text>
-                        <Text>$65.43</Text>
+            {selectedCarDataHistory.map((car: Car) => {
+                return (
+                    <View style={styles.historyItemWrapper}>
+                        <View style={styles.mainInfoWrapper}>
+                            <View>
+                                <Text style={styles.bold}>24-01-2025</Text>
+                                <Text>$65.43</Text>
+                            </View>
+                            <View>
+                                <Text style={styles.bold}>147 954km</Text>
+                                <Text>+412km</Text>
+                            </View>
+                        </View>
+                        <View style={styles.detailsWrapper}>
+                            <View style={styles.detailsWrapper}>
+                                <Text>PB95 $1.64</Text>
+                                <Text>12.5 l/100</Text>
+                            </View>
+                        </View>
                     </View>
-                    <View>
-                        <Text style={styles.bold}>147 954km</Text>
-                        <Text>+412km</Text>
-                    </View>
-                </View>
-                <View style={styles.detailsWrapper}>
-                    <View style={styles.detailsWrapper}>
-                        <Text>PB95 $1.64</Text>
-                        <Text>12.5 l/100</Text>
-                    </View>
-                </View>
-            </View>
+                )
+            })}
         </Wrapper>
     )
 }

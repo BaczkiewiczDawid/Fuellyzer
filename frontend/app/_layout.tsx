e2b10aaf8adbc8959e348c4fcaae62bb1669ca90
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-05-17 19:27:17 +0200

    feat: login screen

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout() {
                 <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
                 <Stack.Screen name="new-car" options={{headerShown: false}}/>
                 <Stack.Screen name="+not-found"/>
+                <Stack.Screen name="login" options={{headerShown: false}} />
             </Stack>
             <StatusBar style="auto"/>
         </ThemeProvider>
---
+++ frontend/app/login.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
+import { Button, View, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import * as AuthSession from 'expo-auth-session'
+
+WebBrowser.maybeCompleteAuthSession()
+
+export default function GoogleLogin() {
+  const [userInfo, setUserInfo] = useState(null)
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '148413152474-8eiq89r6pmusd7gub38v6crqdtt654ui.apps.googleusercontent.com',
+    scopes: ['profile', 'email'],
+    redirectUri: AuthSession.makeRedirectUri({
+      scheme: 'fuellyzer'
+    }),
+  })
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response
+      if (authentication?.accessToken) {
+        fetchUserInfo(authentication.accessToken)
+      }
+    }
+  }, [response])
+
+  const fetchUserInfo = async (token: string) => {
+    const res = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    const user = await res.json()
+    setUserInfo(user)
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button
+        title="Zaloguj się przez Google"
+        disabled={!request}
+        onPress={() => promptAsync()}
+      />
+      {userInfo && (
+        <View style={{ marginTop: 20 }}>
+          {/* <Text>Witaj {userInfo.name}</Text>
+          <Text>Email: {userInfo.email}</Text> */}
+          <Text>Bruh</Text>
+        </View>
+      )}
+    </View>
+  )
+}
